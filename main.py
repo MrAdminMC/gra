e297f6c837b10eca06e72b3b9ecc436f3ead4d2c
MrAdminMC <140478537+MrAdminMC@users.noreply.github.com>
2023-10-21 02:04:00 -0500

    Dz

---
+++ main.py
@@ -0,0 +1,119 @@
+
+#a = input ("Enter first number: ")
+#b = input ("Enter second number: ")
+#c = input (" [a = add]"
+ #          " [b = subtruct]"
+#           " [c = multiply]"
+#           " [d = divide]")
+#if c == "a":
+ #   res = int(a) + int(b)
+ #   print(res)
+#if c == "b":
+ #   res = int(a) - int(b)
+ #   print(res)
+#if c == "c":
+ #   res = int(a) * int(b)
+ #   print(res)
+#if c == "d":
+ #   res = int(a) / int(b)
+ #   print(res)
+
+
+
+
+
+# n = 5
+# while n > 0:
+#      n += 1
+#      print(n)
+
+
+# import random
+#
+# num = random.randint(1, 10)
+# print(num)
+# guess = input("guess a number between 1 and 10: ")
+# if guess == num:
+#         print("congratulations! you won!")
+# else:
+#         print("nope, sorry. try again!")
+
+# import turtle
+# t = turtle.Turtle()
+# t.speed(3)
+# a = input("what color do you want?:")
+# red = t.color("red")
+# orange = t.color("orange")
+# yellow = t.color("yellow")
+# green = t.color("green")
+# blue = t.color("blue")
+# purple = t.color("purple")
+# pink = t.color("pink")
+# if a == red:
+#     t.color("red")
+# if a == orange:
+#     t.color("orange")
+# if a == yellow:
+#     t.color("yellow")
+# if a == green:
+#     t.color("green")
+# if a == blue:
+#     t.color("blue")
+# if a == purple:
+#     purple = t.color("purple")
+# if a == pink:
+#     pink = t.color("pink")
+# t.forward(1000)
+
+
+
+
+
+
+
+# question = (input("Для якої пори ви хочете одяг?: "))
+# if question == "зима":
+#     print("куртку, шарф, пальто, чоботи, шапку, штани.")
+# if question == "весна":
+#     print("светер, легкі штани, шарф, шапку.")
+# if question == "літо":
+#     print("шорти, кепку, футболку, сандалі.")
+# if question == "осінь":
+#     print("шарф, пальто, чоботи, шапку.")
+
+
+a = int(input("чвведіть число: "))
+sum = 0
+i = 0
+
+while sum <= 1000:
+    print(sum)
+    sum += a
+    i += 1
+print("num: ",i )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
